e44b16d99061f1ba010664e62349f1b048227197
Arthur Vickers <ajcvickers@hotmail.com>
2018-02-01 06:58:27 -0800

    Add events for entity state changes
    Issue #10895 (Part of #626)

--- src/EFCore/ChangeTracking/Internal/StateManager.cs
+++ src/EFCore/ChangeTracking/Internal/StateManager.cs
@@ -932,5 +932,39 @@ namespace Microsoft.EntityFrameworkCore.ChangeTracking.Internal
                 entry.AcceptChanges();
             }
         }
+
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public event Action<object, EntityTrackedEventArgs> Tracked;
+
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public virtual void OnTracked([NotNull] InternalEntityEntry internalEntityEntry, bool fromQuery)
+        {
+            var @event = Tracked;
+
+            @event?.Invoke(Context.ChangeTracker, new EntityTrackedEventArgs(internalEntityEntry, fromQuery));
+        }
+
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public event Action<object, EntityStateEventArgs> StateChanged;
+
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public virtual void OnStateChanged([NotNull] InternalEntityEntry internalEntityEntry, EntityState oldState)
+        {
+            var @event = StateChanged;
+
+            @event?.Invoke(Context.ChangeTracker, new EntityStateEventArgs(internalEntityEntry, oldState, internalEntityEntry.EntityState));
+        }
     }
 }
