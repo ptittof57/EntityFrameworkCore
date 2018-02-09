a22fcbaf8015c7fd98b07d12b07a27b91bef9428
Christophe HEISER <ptit_tof57@hotmail.fr>
2018-02-09 22:15:42 +0100

    Update InMemoryIntegerValueGenerator.cs
    Add set current identity try to allow us to reset or set identity issue

--- src/EFCore.InMemory/ValueGeneration/Internal/InMemoryIntegerValueGenerator.cs
+++ src/EFCore.InMemory/ValueGeneration/Internal/InMemoryIntegerValueGenerator.cs
@@ -28,5 +28,14 @@ namespace Microsoft.EntityFrameworkCore.ValueGeneration.Internal
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
         public override bool GeneratesTemporaryValues => false;
+        
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public void SetCurrentIdentity(long identity)
+        { 
+            this._current = identity;
+        }
     }
 }
